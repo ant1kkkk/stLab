1a923b43963c3b8f28c97e7bd7490b6ad8c653b5
ant1kkkk <naziii1337@gmail.com>
2021-10-11 23:14:09 +0300

    Exercise2Full

---
+++ Ex2/Ex2/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ex2
+{
+    class Employee : Person
+    {
+        public string Specialization { get; set; }
+        public int WorkerDay { get; set; }
+        public int NumberOfDays { get; set; }
+        public decimal SalaryPerHour { get; set; }
+        public decimal Salary { get; set; }
+
+        public decimal GetSalary()
+        {
+            Salary = ((SalaryPerHour - GetDeduction(SalaryPerHour))*WorkerDay) * NumberOfDays;
+            return Salary;
+        }
+        public static decimal GetDeduction(decimal salaryPerHour)
+        {
+            return (salaryPerHour * 14) / 100;
+        }
+
+        public void InformationAboutEmployee(params string[] information)
+        {
+            foreach (var str in information)
+                Console.Write($"{str} ");
+        }
+    }
+}
--- Ex2/Ex2/Person.cs
+++ Ex2/Ex2/Person.cs
@@ -1,7 +1,29 @@
-﻿namespace Ex2
+﻿using System;
+
+namespace Ex2
 {
-    public class Person
+    abstract class Person
     {
-        
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void PrintPerson(string name)
+        {
+            Console.WriteLine($"You're: {name}");
+        }
+
+        public void PrintPerson(string firstName, string lastName)
+        {
+            Console.WriteLine($"You're {firstName} {lastName}");
+        }
+
+        public void GetPirson()
+        {
+            Console.Write("\nEnter your first name: ");
+            FirstName = Console.ReadLine();
+            Console.Write("Enter your last name: ");
+            LastName = Console.ReadLine();
+            Console.WriteLine($"You're {FirstName} {LastName}");
+        }
     }
 }
